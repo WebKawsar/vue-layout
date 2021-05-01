6ff542f963c0699777ceab153005d9e732279a00
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2021-05-01 18:06:07 +0600

    problem solved

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,103 @@
 <template>
-<!-- App.vue -->
-<v-app>
+  <v-app id="inspire">
+
+    <!-- Top Navigation Bar -->
+    <v-app-bar
+      app
+      clipped-left
+      flat
+      height="72"
+    >
+    </v-app-bar>
 
 
-  <v-navigation-drawer
-    v-model="drawer"
-    :mini-variant.sync="mini"
-    permanent
-    app
-  >
-    <v-list-item class="px-2">
-      <v-list-item-avatar>
-        <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-      </v-list-item-avatar>
+    <!--Side Navigation drawer -->
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      permanent
+      app
+      clipped
+    >
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
 
-      <v-list-item-title>John Leider</v-list-item-title>
+        <v-list-item-title>John Leider</v-list-item-title>
 
-      <v-btn
-        icon
-        @click.stop="mini = !mini"
-      >
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
-    </v-list-item>
-
-    <v-divider></v-divider>
-
-    <v-list dense>
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        link
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
+        <v-btn
+          icon
+          @click.stop="mini = !mini"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
       </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
 
+    <!-- Conetnt -->
+    <v-main>
+      <template>
+        <v-container class="grey lighten-5" fluid>
+          <v-row no-gutters>
+            <v-col
+              cols="12"
+              sm="6"
+              md="8"
+            >
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                .col-12 .col-sm-6 .col-md-8
+              </v-card>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+            >
+              <v-card
+                class="pa-2"
+                outlined
+                tile
+              >
+                .col-6 .col-md-4
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
+    </v-main>
 
-  <v-app-bar app>
-    <!-- -->
-  </v-app-bar>
 
-  <!-- Sizes your content based upon application components -->
-  <v-main>
-
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-
-      <!-- If using vue-router -->
-      <router-view></router-view>
-
-      
-    </v-container>
-  </v-main>
-
-  <v-footer app>
-    <!-- -->
-  </v-footer>
-</v-app>
+    <!-- Footer -->
+    <v-footer app>
+      <p>Footer Section</p>
+    </v-footer>
+  </v-app>
 </template>
+
 
 <script>
   export default {
